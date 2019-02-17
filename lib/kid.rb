35953ff6977325791077f4809083498b29fd01a0
Marci Clark <marcig612@gmail.com>
2019-02-17 14:21:44 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid 
+  include Dance 
+  
+  extend MetaDancing
+end 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end 
+end
